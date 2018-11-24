a4794b972e2eb0e8d847fbe5a1c942d3f115f9b8
Maxpoto <maxpoto@gmail.com>
2018-11-24 18:24:31 +0100

    modifica testo

--- provino.py
+++ provino.py
@@ -1,10 +1,13 @@
-print("CIAO a tutti")
+import random
+
+print("CIAO a tutti " + str(random.random()))
+
 selezione = input("Give me a number : ")
 try:
     if int(selezione) > 10:
-        print("Valid number")
+        print("Valid number %d" % int(selezione))
     else:
-        print("Little number")
+        print("Little number %d" & int(selezione))
 except ValueError as err:
     print("ERROR : " + str(err))
 
