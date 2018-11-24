c53c704dacebbb14165a8b7ee1a624e1d37f42ba
Maxpoto <maxpoto@gmail.com>
2018-11-24 17:53:42 +0100

    modifica totale

--- provino.py
+++ provino.py
@@ -1,1 +1,10 @@
 print("CIAO a tutti")
+selezione = input("Give me a number : ")
+try:
+    if int(selezione) > 10:
+        print("Valid number")
+    else:
+        print("Little number")
+except ValueError as err:
+    print("ERROR : " + str(err))
+
