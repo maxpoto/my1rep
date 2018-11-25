8d4104a0b3b9fe8f1fd822e3e69f7c2dabed8d61
Maxpoto <maxpoto@gmail.com>
2018-11-25 16:43:40 +0100

    modifica testo

--- provino.py
+++ provino.py
@@ -1,6 +1,45 @@
 import random
+import xml.etree.ElementTree as ElTr
+import json
 
+dataj='''
+[
+{ "id" : "001",
+"x" : "2",
+"name" : "Chuck"
+} ,
+{ "id" : "009",
+"x" : "7",
+"name" : "Chuck"
+}
+]'''
+
+data = '''
+<person>
+<name>Chuck</name>
+<phone type="intl">
++1 734 303 4456
+</phone>
+<email hide="yes"/>
+</person>'''
+
+info = json.loads(dataj)
+print('User count:', len(info))
+for item in info:
+    print('Name', item['name'])
+    print('Id', item['id'])
+    print('Attribute', item['x'])
+
+print()
+
+tree = ElTr.fromstring(data)
+print('Name:', tree.find('name').text)
+print('Attr:', tree.find('email').get('hide'))
 print("CIAO a tutti " + str(random.random()))
+
+elenco = ["Ciao", 23, 12, "WWW"]
+dizionario = {"cazzi": 1, "fighe": 0, "culi": 3}
+tupla = ("pippo", "baudo", 78)
 
 selezione = input("Give me a number : ")
 try:
@@ -11,3 +50,4 @@ try:
 except ValueError as err:
     print("ERROR : " + str(err))
 
+print("CIAO \n".rstrip())
